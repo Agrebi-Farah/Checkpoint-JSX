c03ebfe780c30b3d11396de0d439d4541ffe4b2a
farah <farahagrebi1996@gmail.com>
2021-12-13 14:49:34 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+
 import './App.css';
+import farah from './img2.jpeg' ;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Main">
+    
+    
+    <div style ={{border:'solid 1px black', maxWidth:'100vw'}}/>
+    <h1 className="title">FARAH</h1>
+    <br/>
+    <br/>
+    <img src={farah} alt='image de farah'/>
+    <br/>
+    <br/>
+    <br/>
+    
+   {<iframe width="853" height="480" src="https://www.youtube.com/embed/Mupom-sgjAU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>  }
+   <br/>
+   <br/>
+   <br/>
+    <img src ="/img1.jpg" alt ="C:\Users\Administrateur\Desktop\CHECKPOINT JSX\checkpoint\public\img1.jpg" />
+    
     </div>
-  );
-}
-
-export default App;
+      );
+    }
+    
+    export default App;
